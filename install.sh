4b97c395d49de88b2c703ac4f88ec11f80e2f87b
ViktorF <martin.foedinger@protonmail.com>
2025-03-07 22:42:25 +0100

    install.sh an Python venv Installation anpassen

--- install.sh
+++ install.sh
@@ -3,33 +3,30 @@
 sudo apt-get update
 sudo apt-get upgrade
 # Installiert Influxdb
-apt-get -y install influxdb
-apt-get -y install influxdb-client
+#apt-get -y install influxdb
+#apt-get -y install influxdb-client
 # Startet den Influxdb service
-systemctl start influxdb
+#systemctl start influxdb
 # Erstellen der Datenbank
-influx -execute "create database SmartMeter"
+#influx -execute "create database SmartMeter"
 # Installiert Grafana
-wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
-echo "deb https://packages.grafana.com/oss/deb stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
-sudo apt-get update
-sudo apt-get install grafana
+#wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
+#echo "deb https://packages.grafana.com/oss/deb stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
+#sudo apt-get update
+#sudo apt-get install grafana
 ### Legt Grafana in den Autostart
-sudo /bin/systemctl enable grafana-server
+#sudo /bin/systemctl enable grafana-server
 ### Start Grafana Server
-sudo /bin/systemctl start grafana-server
-sudo apt-get update
+#sudo /bin/systemctl start grafana-server
+#sudo apt-get update
 # Installiert Python3
-apt install python3 idle3
-sudo apt-get install python3-pip
+sudo apt install python3 idle3
+sudo apt install python3-pip
 # Installation von Python Paketen
-sudo pip3 install gurux-dlms --break-system-packages
-sudo pip3 install beautifulsoup4 --break-system-packages
-sudo pip3 install paho-mqtt --break-system-packages
-sudo apt-get install libxslt-dev --break-system-packages
-sudo pip3 install pyserial --break-system-packages
-sudo pip3 install cryptography --break-system-packages
-sudo pip install influxdb --break-system-packages
-sudo pip3 install html5lib --break-system-packages
-sudo apt install python3-pycryptodome
-sudo pip3 install sdnotify --break-system-packages
+sudo apt install libxslt-dev
+#für die restlichen Python Packages folgende Befehle ausführen
+#python -m venv smartmeter
+#./smartmeter/bin/activate
+#pip install -U -r requirements.txt
+#
+#für das SystemD Service die smartmeter-venv.service nach /etc/systemd/system/ kopieren
